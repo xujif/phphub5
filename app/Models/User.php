0ccd7b1c38527a2f415c6344d104550bd7e542ad
xujif <i@xujif.com>
2016-08-19 15:53:41 +0800

    修改

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,8 +50,8 @@ class User extends Model implements AuthenticatableContract,
             $driver = $user['register_source'];
             SiteStatus::newUser($driver);
 			if($driver != 'kuaiyudian'){
-				$user['verification_token'] = null;
-				$user['verified'] = 1;
+				$user->verification_token = null;
+				$user->verified = 1;
 				$user->save();
 			}
 			else{
